8f0a99bf5b86467af8ac56cb19dbfaf6be839808
Wander Lairson Costa <wander.lairson@gmail.com>
2011-10-16 13:07:33 -0200

    Removed old test firmware.

--- tests/pic18f4550/Microchip Solutions/TestFirmware/Firmware/superClean.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-.\projects\VStudio\Utility\RegexClean -m"\.(\$\$\$|bkx|cce|cod|cof|err|hex|lde|i|lde|lst|obj|o|rlf|sym|sdb|wat|mcs|mptags|tagsrc|map|elf|ncb|resharper|suo|user)$"
-IF EXIST ".\Objects\" rd Objects /S /Q
-.\projects\VStudio\Utility\RegexClean -r -d -m"\\_ReSharper\."
-.\projects\VStudio\Utility\RegexClean -r -d -m"\\(Release|Debug|temp|output)$"
