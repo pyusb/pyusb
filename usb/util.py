pyusb/pyusb

commit 01b5d9e6ce26ec3ed934195166deecb12be02ac6

Fix array creation on Python 3.4.
On Python 3.4, you cannot mix 'B' typecode with str. We use a tuple
instead for compability with older versions of Python.

closes #51.

usb/util.py
@@ -147,7 +147,7 @@ def create_buffer(length):
     call. This function creates a compatible sequence buffer
     of the given length.
     """
-    return array.array('B', '\x00' * length)
+    return array.array('B', (0,) * length)
 
 def find_descriptor(desc, find_all=False, custom_match=None, **args):
     r"""Find an inner descriptor.

commit bd949ed5c867315ba957df142c85cca050bd151c

Fix performance regression.
Commit 01b5d9e6 introduced a performance regression: creating a tuple of
zeros is much slower than a string. For Python 2 we have to use
'\x00' * length, but for Python 3 we need b'\x00' * length.

We use a little dirty hack with encode('utf-8') method to keep
compatibility between Python 2 and 3.

usb/util.py
@@ -86,6 +86,9 @@ _ENDPOINT_DIR_MASK = 0x80
 _ENDPOINT_TRANSFER_TYPE_MASK = 0x03
 _CTRL_DIR_MASK = 0x80
 
+# For compatibility between Python 2 and 3
+_dummy_s = '\x00'.encode('utf-8')
+
 def endpoint_address(address):
     r"""Return the endpoint absolute address.
 
@@ -147,7 +150,7 @@ def create_buffer(length):
     call. This function creates a compatible sequence buffer
     of the given length.
     """
-    return array.array('B', (0,) * length)
+    return array.array('B', _dummy_s * length)
 
 def find_descriptor(desc, find_all=False, custom_match=None, **args):
     r"""Find an inner descriptor.