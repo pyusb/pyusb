13bc3041bbcdf3a7a6931e0b670440105d8f80a4
mdxs <van.wijgerden@mdxs.net>
2019-11-29 17:03:36 +0100

    Updated description for PyPI in setup.py (#280)
    Some grammar and capitalization fixed.

--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ setup(
 """
 PyUSB offers easy USB devices communication in Python.
 It should work without additional code in any environment with
-Python >= 2.4, ctypes and an pre-built usb backend library
-(currently, libusb 0.1.x, libusb 1.x, and OpenUSB).
+Python >= 2.4, ctypes and a pre-built USB backend library
+(currently: libusb 0.1.x, libusb 1.x, or OpenUSB).
 """,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
