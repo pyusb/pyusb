3ea79b01af08c1e8b2e0096a266c2cbd347738db
Jonas Malaco <jonas@protocubo.io>
2022-11-10 19:56:24 -0300

    util: remove left over python 2 compatibility workaround

--- usb/util.py
+++ usb/util.py
@@ -88,9 +88,6 @@ _ENDPOINT_DIR_MASK = 0x80
 _ENDPOINT_TRANSFER_TYPE_MASK = 0x03
 _CTRL_DIR_MASK = 0x80
 
-# For compatibility between Python 2 and 3
-_dummy_s = '\x00'.encode('utf-8')
-
 # speed type
 SPEED_LOW = 1
 SPEED_FULL = 2
@@ -159,7 +156,7 @@ def create_buffer(length):
     call. This function creates a compatible sequence buffer
     of the given length.
     """
-    return array.array('B', _dummy_s * length)
+    return array.array('B', bytes(length))
 
 def find_descriptor(desc, find_all=False, custom_match=None, **args):
     r"""Find an inner descriptor.
