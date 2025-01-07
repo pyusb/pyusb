pyusb/pyusb

commit 3ea79b01af08c1e8b2e0096a266c2cbd347738db

util: remove left over python 2 compatibility workaround

usb/util.py
@@ -88,9 +88,6 @@ _ENDPOINT_DIR_MASK = 0x80
 _ENDPOINT_TRANSFER_TYPE_MASK = 0x03
 _CTRL_DIR_MASK = 0x80
 
-# For compatibility between Python 2 and 3
-_dummy_s = '\x00'.encode('utf-8')
-
 # speed type
 SPEED_LOW = 1
 SPEED_FULL = 2
@@ -159,7 +156,7 @@ def create_buffer(length):
     call. This function creates a compatible sequence buffer
     of the given length.
     """
-    return array.array('B', _dummy_s * length)
+    return array.array('B', bytes(length))
 
 def find_descriptor(desc, find_all=False, custom_match=None, **args):
     r"""Find an inner descriptor.

commit 304a94438d5f72086aec2d30d8ea4e1a1c810d63

util: raise TypeError if create_buffer doesn't receive a length
`Device.ctrl_transfer` depends on `create_buffer` raising a `TypeError`
if its parameter is not a valid length. In particular, if an array is
passed in, `create_buffer` must *not* return a copy of it.

Commit 3ea79b01af08 accidentally broke this and, consequently, any calls
to `ctrl_transfer` passing in a buffer. Thanks @kauwua for reporting the
issue and discussing the potential solutions.

Note that `length * b'\x00'` is actually faster than `bytes(length)`
(tested with Python 3.13 on x86_64).

Fixes: #528
Fixes: 3ea79b01af08 ("util: remove left over python 2 compatibility workaround")

usb/util.py
@@ -156,7 +156,8 @@ def create_buffer(length):
     call. This function creates a compatible sequence buffer
     of the given length.
     """
-    return array.array('B', bytes(length))
+    # Return an array with `length` zeros or raise a `TypeError`.
+    return array.array('B', length * b"\x00")
 
 def find_descriptor(desc, find_all=False, custom_match=None, **args):
     r"""Find an inner descriptor.

tests/test_util.py
@@ -28,12 +28,15 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import utils
+import array
 import unittest
-from usb.util import *
-from devinfo import *
-from usb._debug import methodtrace
+import utils
+
 import usb.backend
+from usb._debug import methodtrace
+from usb.util import *
+
+from devinfo import *
 
 class _ConfigurationDescriptor(object):
     def __init__(self, bConfigurationValue):
@@ -145,6 +148,16 @@ class UtilTest(unittest.TestCase):
         self.assertEqual(build_request_type(CTRL_IN, CTRL_TYPE_RESERVED, CTRL_RECIPIENT_INTERFACE), 0xe1)
         self.assertEqual(build_request_type(CTRL_IN, CTRL_TYPE_RESERVED, CTRL_RECIPIENT_ENDPOINT), 0xe2)
         self.assertEqual(build_request_type(CTRL_IN, CTRL_TYPE_RESERVED, CTRL_RECIPIENT_OTHER), 0xe3)
+
+    @methodtrace(utils.logger)
+    def test_create_buffer_with_int(self):
+        self.assertEqual(create_buffer(3), array.array('B', [0, 0, 0]))
+
+    @methodtrace(utils.logger)
+    def test_create_buffer_with_other_types(self):
+        self.assertRaises(TypeError, lambda: create_buffer(None))
+        self.assertRaises(TypeError, lambda: create_buffer([10, 20, 30]))
+        self.assertRaises(TypeError, lambda: create_buffer(array.array('B', [10, 20, 30])))
 
 def get_suite():
     suite = unittest.TestSuite()